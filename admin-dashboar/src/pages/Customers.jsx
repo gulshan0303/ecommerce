81c14376f2b371ea11aedabfbc7a980f9cc00511
Gulshan kathare <gulshan03kathare@gmail.com>
2023-04-07 22:09:51 +0530

    modified:   admin-dashboar/src/pages/Customers.jsx 	modified:   admin-dashboar/package-lock.json 	modified:   admin-dashboar/package.json 	modified:   admin-dashboar/public/index.html 	modified:   admin-dashboar/src/App.js 	modified:   admin-dashboar/src/app/Store.js 	modified:   admin-dashboar/src/components/CustomInput.jsx 	modified:   admin-dashboar/src/components/mainLayout/MainLayout.jsx 	modified:   admin-dashboar/src/features/auth/authServices.js 	modified:   admin-dashboar/src/features/blogcategory/blogCategoryServices.js 	modified:   admin-dashboar/src/features/blogcategory/blogCategorySlice.jsx 	modified:   admin-dashboar/src/features/blogs/Services.js 	modified:   admin-dashboar/src/features/blogs/blogSlice.jsx 	modified:   admin-dashboar/src/features/brand/Services.js 	modified:   admin-dashboar/src/features/brand/brandSlice.js 	modified:   admin-dashboar/src/features/color/Services.js 	modified:   admin-dashboar/src/features/color/colorSlice.js 	modified:   admin-dashboar/src/features/coupon/Services.js 	deleted:    admin-dashboar/src/features/coupon/services.Slice.js 	modified:   admin-dashboar/src/features/customers/Services.js 	modified:   admin-dashboar/src/features/customers/customersSlice.js 	modified:   admin-dashboar/src/features/enquiry/EnqSlice.js 	modified:   admin-dashboar/src/features/enquiry/Services.js 	modified:   admin-dashboar/src/features/prodcategory/Services.js 	modified:   admin-dashboar/src/features/prodcategory/proSlice.js 	modified:   admin-dashboar/src/features/product/Services.js 	modified:   admin-dashboar/src/features/product/productSlice.js 	modified:   admin-dashboar/src/features/upload/Services.js 	modified:   admin-dashboar/src/features/upload/uploadSlice.js 	modified:   admin-dashboar/src/pages/Addblog.jsx 	modified:   admin-dashboar/src/pages/Addblogcat.jsx 	modified:   admin-dashboar/src/pages/Addbrand.jsx 	modified:   admin-dashboar/src/pages/Addcat.jsx 	modified:   admin-dashboar/src/pages/Addcolor.jsx 	modified:   admin-dashboar/src/pages/Addcoupon.jsx 	modified:   admin-dashboar/src/pages/Addproduct.jsx 	modified:   admin-dashboar/src/pages/Blogcatlist.jsx 	modified:   admin-dashboar/src/pages/Bloglist.jsx 	modified:   admin-dashboar/src/pages/Brandlist.jsx 	modified:   admin-dashboar/src/pages/Categorylist.jsx 	modified:   admin-dashboar/src/pages/Colorlist.jsx 	modified:   admin-dashboar/src/pages/Couponlist.jsx 	modified:   admin-dashboar/src/pages/Dashboard.jsx 	modified:   admin-dashboar/src/pages/Enquiries.jsx 	modified:   admin-dashboar/src/pages/ForgotPassword.jsx 	modified:   admin-dashboar/src/pages/Order.jsx 	modified:   admin-dashboar/src/pages/Productlist.jsx 	modified:   admin-dashboar/src/pages/ResetPassword.jsx 	modified:   admin-dashboar/src/pages/ViewEnq.jsx 	modified:   admin-dashboar/src/pages/Vieworder.jsx 	modified:   admin-dashboar/src/pages/login/Login.jsx 	modified:   backend/controller/order.js 	modified:   backend/index.js 	modified:   backend/middleware/uploadImage.js 	modified:   backend/model/order.js 	modified:   backend/routes/blog.js 	modified:   backend/routes/brand.js 	modified:   backend/routes/product.js 	modified:   backend/routes/userRoutes.js 	modified:   backend/utils/clodinary.js

--- admin-dashboar/src/pages/Customers.jsx
+++ admin-dashboar/src/pages/Customers.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Table } from "antd";
+import { getUsers } from "../features/customers/customersSlice";
+import axios from "axios";
 
 const columns = [
   {
@@ -22,15 +25,41 @@ const columns = [
 ];
 
 const Customers = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsers());
+    const findApi = async() => {
+      const res = await axios.get(`http://localhost:8484/api/v1/blogcategory/all`);
+      console.log('res.data--', res.data)
+       return res.data;
+    }
+    findApi();
+  }, []);
+  const customerstate = useSelector(
+    (state) => state.customer?.customers?.getAllUser
+  );
   const data1 = [];
+  if (customerstate && customerstate.length) {
+    for (let i = 0; i < customerstate.length; i++) {
+      if (customerstate[i].role !== "admin") {
+        data1.push({
+          key: i + 1,
+          name: customerstate[i].firstName + " " + customerstate[i].lastName,
+          email: customerstate[i].email,
+          mobile: customerstate[i].mobile,
+        });
+      }
+    }
+  }
+
   return (
     <>
-     <div className="mb-4 title">
-      Customers
-      <Table columns={columns} dataSource={data1}/>
-     </div>
+      <div className="mb-4 title">
+        Customers
+        <Table columns={columns} dataSource={data1} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Customers
+export default Customers;
