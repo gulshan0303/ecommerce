b408b3ac681bdb8a677c0f2066a15ec85e535f43
Gulshan kathare <gulshan03kathare@gmail.com>
2023-04-08 10:52:11 +0530

    modified:   admin-dashboar/src/features/auth/authServices.js 	modified:   admin-dashboar/src/pages/Order.jsx 	modified:   admin-dashboar/src/pages/Vieworder.jsx 	modified:   backend/controller/order.js 	modified:   backend/model/order.js 	modified:   backend/routes/userRoutes.js

--- admin-dashboar/src/features/auth/authServices.js
+++ admin-dashboar/src/features/auth/authServices.js
@@ -5,22 +5,17 @@ const login = async (user) => {
   const response = await axios.post(`${base_url}auth/admin-login`, user);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
-    console.log('response.data', response.data)
+    console.log("response.data", response.data);
   }
   return response.data;
 };
 const getOrders = async () => {
   const response = await axios.get(`${base_url}auth/order/all`, config);
-   console.log('response', response)
+  console.log("response", response);
   return response.data;
 };
 const getOrder = async (id) => {
-  const response = await axios.post(
-    `${base_url}auth/order/${id}`,
-    "",
-    config
-  );
-
+  const response = await axios.get(`${base_url}auth/order/${id}`, config);
   return response.data;
 };
 
